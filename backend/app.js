a95fb5b0e28931489dc64b7d5f5fce16c7972dfb
Sutharshana <71762133048@cit.edu.in>
2024-03-27 12:48:59 +0530

    Update

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: "https://rreservation.netlify.app/",
     methods: ["POST"],
     credentials: true,
   })
